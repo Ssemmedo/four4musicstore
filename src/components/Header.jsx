e962e64b8d4003477b6643c77087519545d51d41
Sandro Semedo <ssemdo44@gmail.com>
2025-05-20 21:19:02 -0100

    Finishing desktop navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import logo from "../logo.svg";
+import "../css/header.css"
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="App-logo">
+        <a href="/">
+          <img src={logo} alt="Four4 Music Store"/>
+        </a>
+      </div>
+      <div className="header-options">
+        <ul>
+          <li>
+            <a href="#home">Inicio</a>
+          </li>
+          <li>
+            <a href="#produtos">Produtos</a>
+          </li>
+          <li>
+            <a href="#contato">Contato</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
